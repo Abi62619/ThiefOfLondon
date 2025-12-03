3306ce4cf65d3c60a2762da21f22970c0daec9ac
Abi <reidabi6@gmail.com>
2025-12-03 15:17:35 +0000

    sjhfiuedbnf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,80 +3,89 @@ using UnityEngine.InputSystem;
 
 public class PlayerControllerNew : MonoBehaviour
 {
-    // Reference to your Input Action Asset
     public InputActionAsset inputActionAsset;
 
-    // Input Actions // Action to bind and use
     private InputAction moveAction;
     private InputAction interactAction;
     private InputAction jumpAction;
 
-    // Movement variables
     private Vector2 moveInput;
     public float moveSpeed = 5f;
+    public float rotationSpeed = 10f;
 
     private void OnEnable()
     {
-        // Find the specific action map and actions
-        var actionMap = inputActionAsset.FindActionMap("Player");  // Replace "Player" with the name of your action map
-        moveAction = actionMap.FindAction("Move");                // Replace "Move" with the name of your move action
-        interactAction = actionMap.FindAction("Interact");        // Replace "Interact" with the name of your interact action
+        var actionMap = inputActionAsset.FindActionMap("Player");
+        moveAction = actionMap.FindAction("Move");
+        interactAction = actionMap.FindAction("Interact");
         jumpAction = actionMap.FindAction("Jump");
 
-        // Enable actions
         moveAction.Enable();
         interactAction.Enable();
         jumpAction.Enable();
 
-
-        // Subscribe to input events
         moveAction.performed += OnMove;
-        moveAction.canceled += OnMove;  // Handle when movement input stops
+        moveAction.canceled += OnMove;
         interactAction.performed += OnInteract;
         jumpAction.performed += OnJump;
-       
+    }
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //can be deleted 
+        Cursor.visible = false;
+
+        MovePlayer();
+        RotatePlayerTowardMovement();
     }
 
     private void OnDisable()
     {
-        // Unsubscribe from input events
         moveAction.performed -= OnMove;
         moveAction.canceled -= OnMove;
         interactAction.performed -= OnInteract;
         jumpAction.performed -= OnJump;
-        
 
-        // Disable actions
         moveAction.Disable();
         interactAction.Disable();
         jumpAction.Disable();
     }
 
-    // Move input handler
     private void OnMove(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();  // Read the 2D move vector from the input
+        moveInput = context.ReadValue<Vector2>();
     }
 
-    // Interact button handler
     private void OnInteract(InputAction.CallbackContext context)
     {
-        // Perform interaction logic here
         Debug.Log("Interact button pressed!");
     }
 
-    private void OnJump (InputAction.CallbackContext context)
+    private void OnJump(InputAction.CallbackContext context)
     {
         Debug.Log("You Jumped");
     }
 
-    private void Update()
-    {        
-        Vector2 MoveSide = moveAction.ReadValue<Vector2>();
-       /* if (jumpAction.ReadValue<float>()!=0) // Use this if you want continious input
-        {
-            Debug.Log("Jump Continoues");
-        }*/
+    private void MovePlayer()
+    {
+        Vector3 move = new Vector3(-moveInput.x, 0, -moveInput.y);
 
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void RotatePlayerTowardMovement()
+    {
+        if (moveInput.sqrMagnitude > 0.01f)
+        {
+            Vector3 direction = new Vector3(-moveInput.x, 0, -moveInput.y);
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                targetRotation,
+                rotationSpeed * Time.deltaTime
+            );
+        }
     }
 }
