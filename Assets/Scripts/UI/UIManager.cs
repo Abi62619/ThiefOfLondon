3214714306fe62c2fb298073632d574e7b0cfa29
Abi <reidabi6@gmail.com>
2025-12-04 10:13:29 +0000

    vgsfefs

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private int coin = 1;
+
+    [SerializeField] private TextMeshProUGUI CoinText; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
