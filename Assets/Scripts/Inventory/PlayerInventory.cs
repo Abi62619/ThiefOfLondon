517ca0a9675e1249a0bbf1f73aa1c58a13f603b6
Abi <reidabi6@gmail.com>
2026-03-11 15:12:20 +0000

    kk

--- Assets/Scripts/Inventory/PlayerInventory.cs
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -1,10 +1,51 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class PlayerInventory : MonoBehaviour
 {
+    [Header("Input")]
+    public InputActionAsset inputActionAsset;
+
+    private InputAction saveAction;
+    private InputAction loadAction;
+
+    [Header("Items")]
     public List<Item> items = new List<Item>();
     public ItemDatabase database;
+
+    void OnEnable()
+    {
+        var actionMap = inputActionAsset.FindActionMap("Player");
+
+        saveAction = actionMap.FindAction("Save");
+        loadAction = actionMap.FindAction("Load");
+
+        saveAction.performed += OnSave;
+        loadAction.performed += OnLoad;
+
+        saveAction.Enable();
+        loadAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        saveAction.performed -= OnSave;
+        loadAction.performed -= OnLoad;
+
+        saveAction.Disable();
+        loadAction.Disable();
+    }
+
+    void OnSave(InputAction.CallbackContext ctx)
+    {
+        InventorySaveSystem.SaveInventory(this);
+    }
+
+    void OnLoad(InputAction.CallbackContext ctx)
+    {
+        InventorySaveSystem.LoadInventory(this);
+    }
 
     public InventoryData GetSaveData()
     {
@@ -25,6 +66,7 @@ public class PlayerInventory : MonoBehaviour
         foreach (int id in data.itemIds)
         {
             Item item = database.GetItem(id);
+
             if (item != null)
                 items.Add(item);
         }
